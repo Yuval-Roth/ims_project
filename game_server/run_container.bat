15a819b45695b79953b3a725e0478f6d1895bbbd
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-05 12:37:20 +0200

    Added game server api doc

--- game_server/run_container.bat
+++ game_server/run_container.bat
@@ -1,5 +1,5 @@
 call mvn package -DskipTests
-call docker build -t game_server .
-call docker container rm game_server
-start docker run -p 8640:8640 -p 8641:8641/udp --name game_server game_server
+call docker build -t imsproject_game_server .
+call docker container rm imsproject_game_server
+start docker run -p 8640:8640 -p 8641:8641/udp --name imsproject_game_server imsproject_game_server
  
