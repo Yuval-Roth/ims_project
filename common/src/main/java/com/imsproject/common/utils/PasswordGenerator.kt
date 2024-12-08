0be607f1d1503606b770d6907c31f047915b25b3
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-08 03:09:44 +0200

    updates to password generator

--- common/src/main/java/com/imsproject/common/utils/PasswordGenerator.kt
+++ common/src/main/java/com/imsproject/common/utils/PasswordGenerator.kt
@@ -4,16 +4,29 @@ import java.util.*
 
 object PasswordGenerator {
 
-    fun generatePassword(): String {
+    fun generatePassword(length : Int, specialChars : Boolean = true): String {
         val rand = Random()
-        val chars = CharArray(32) { ' ' }
-        val specialChars = "!@#$%^&*()\\-=\\[\\]{};':\"<>?|"
+        val chars = CharArray(length) { ' ' }
+        val specials = "!@#$%^&*()\\-=[]{};':\"<>?|"
         var i = 0
-        while (i < 32) {
-            chars[i++] = (specialChars[rand.nextInt(specialChars.length)])
-            chars[i++] = (rand.nextInt('a'.code, 'z'.code).toChar())
-            chars[i++] = (rand.nextInt('A'.code, 'Z'.code).toChar())
-            chars[i++] = (rand.nextInt('0'.code, '9'.code).toChar())
+        fun next(num : Int) : Char {
+            return when(num % 4) {
+                0 -> {
+                    if (specialChars){
+                        specials[rand.nextInt(specials.length)]
+                    } else {
+                        rand.nextInt('a'.code, 'z'.code).toChar()
+                    }
+                }
+                1 -> rand.nextInt('a'.code, 'z'.code).toChar()
+                2 -> rand.nextInt('A'.code, 'Z'.code).toChar()
+                3 -> rand.nextInt('0'.code, '9'.code).toChar()
+                else -> throw Exception("WTF?")
+            }
+        }
+        while (i < length) {
+            chars[i] = next(i)
+            i++
         }
         chars.shuffle()
         return String(chars)
