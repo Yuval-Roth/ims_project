a62df071a70fd90d220244ded13cccb88ac7d010
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-11 15:43:02 +0200

    Added robust error handling in GameController

--- game_server/src/main/kotlin/com/imsproject/gameServer/GameController.kt
+++ game_server/src/main/kotlin/com/imsproject/gameServer/GameController.kt
@@ -20,18 +20,26 @@ class GameController(private val clientController: ClientController) {
     private val clientToLobby = ConcurrentHashMap<String, String>()
     private val idGenerator = SimpleIdGenerator(2)
 
+    /**
+     * Handles a game request from the manager
+     */
     fun handleGameRequest(request: GameRequest) : String {
-        return when(request.type){
-            Type.GET_ONLINE_PLAYER_IDS -> handleGetOnlinePlayerIds()
-            Type.GET_ALL_LOBBIES -> handleGetAllLobbies()
-            Type.GET_LOBBY -> handleGetLobby(request)
-            Type.CREATE_LOBBY -> handleCreateLobby(request)
-            Type.SET_LOBBY_TYPE -> handleSetLobbyType(request)
-            Type.JOIN_LOBBY -> handleJoinLobby(request)
-            Type.LEAVE_LOBBY -> handleLeaveLobby(request)
-            Type.START_GAME -> handleStartGame(request)
-            Type.END_GAME -> handleEndGame(request)
-            else -> Response.getError("Invalid message type")
+        try{
+            return when(request.type){
+                Type.GET_ONLINE_PLAYER_IDS -> handleGetOnlinePlayerIds()
+                Type.GET_ALL_LOBBIES -> handleGetAllLobbies()
+                Type.GET_LOBBY -> handleGetLobby(request)
+                Type.CREATE_LOBBY -> handleCreateLobby(request)
+                Type.SET_LOBBY_TYPE -> handleSetLobbyType(request)
+                Type.JOIN_LOBBY -> handleJoinLobby(request)
+                Type.LEAVE_LOBBY -> handleLeaveLobby(request)
+                Type.START_GAME -> handleStartGame(request)
+                Type.END_GAME -> handleEndGame(request)
+                else -> Response.getError("Invalid message type")
+            }
+        } catch(e: Exception){
+            log.error("Error handling game request", e)
+            return Response.getError(e)
         }
     }
 
@@ -41,99 +49,198 @@ class GameController(private val clientController: ClientController) {
     }
 
     /**
+     * Handles a game request from a client
      * @throws IllegalArgumentException if anything is wrong with the message
      */
     @Throws(IllegalArgumentException::class)
     fun handleGameRequest(clientHandler: ClientHandler, request: GameRequest) {
         when (request.type) {
-            Type.TOGGLE_READY -> handleToggleReady(clientHandler, request)
+            Type.TOGGLE_READY -> handleToggleReady(clientHandler)
             else -> throw IllegalArgumentException("Invalid message type: ${request.type}")
         }
     }
 
+    /**
+     * @throws IllegalArgumentException if anything is wrong with the message
+     */
+    @Throws(IllegalArgumentException::class)
     fun handleGameAction(clientHandler: ClientHandler, action: GameAction) {
-        val game = clientIdToGame[clientHandler.id] ?: throw IllegalArgumentException("Game not found")
+        // ========= parameter validation ========= |
+        val game = clientIdToGame[clientHandler.id] ?: run{
+            log.debug("handleGameAction: Game not found for client: {}",clientHandler.id)
+            throw IllegalArgumentException("Client not in game")
+        }
+        // ======================================== |
+
         game.handleGameAction(clientHandler, action)
     }
 
-    private fun handleToggleReady(clientHandler: ClientHandler, request: GameRequest) {
-        val lobby = lobbies[clientHandler.id] ?: throw IllegalArgumentException("Player not in lobby")
+    private fun handleToggleReady(clientHandler: ClientHandler) {
+        log.debug("handleToggleReady() with clientId: {}",clientHandler.id)
+
+        // ========= parameter validation ========= |
+        val lobby = lobbies[clientHandler.id] ?: run {
+            log.debug("handleToggleReady: Lobby not found for client: {}",clientHandler.id)
+            throw IllegalArgumentException("Player not in lobby")
+        }
+        // ======================================== |
+
         val success = lobby.toggleReady(clientHandler.id)
-        if(! success){
+        if (success) {
+            log.debug("handleToggleReady() successful")
+        } else {
+            // should not happen
+            log.error("handleToggleReady() failed: Lobby found for player but toggle ready failed")
             throw IllegalArgumentException("Toggle ready failed")
         }
     }
 
     private fun handleGetAllLobbies() : String {
+        log.debug("handleGetAllLobbies()")
+
         val lobbiesInfo = lobbies.values.map { it.getInfo().toJson() }
         return Response.getOk(lobbiesInfo)
     }
 
     private fun handleGetLobby(request: GameRequest) : String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
+        log.debug("handleGetLobby() with lobbyId: {}",request.lobbyId)
+
+        // ========= parameter validation ========= |
+        val errorMsg = "Missing the following parameters: lobbyId"
+        val lobbyId = request.lobbyId ?: run {
+            log.debug("handleGetLobby: $errorMsg")
+            return Response.getError(errorMsg)
+        }
+        // === check if the lobby exists === |
+        val lobby = lobbies[lobbyId] ?: run {
+            log.debug("handleGetLobby: Lobby not found")
+            return Response.getError("Lobby not found")
+        }
+        // ======================================== |
+
         return Response.getOk(lobby.getInfo().toJson())
     }
 
     private fun handleLeaveLobby(request: GameRequest): String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val clientId = request.playerId ?: return Response.getError("Player ID not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
-        val clientHandler = clientController.getByClientId(clientId) ?: return Response.getError("Player not found")
+        log.debug("handleLeaveLobby() with lobbyId: {}, playerId: {}",request.lobbyId,request.playerId)
+        
+        // ========= parameter validation ========= |
+        val missingParams = mutableListOf<String>()
+        val errorMsg = "Missing the following parameters: "
+        /*(1)*/ val lobbyId = request.lobbyId ?: run { missingParams.add("lobbyId") ; null }
+        /*(2)*/ val clientId = request.playerId ?: run { missingParams.add("playerId") ; null }
+        if(lobbyId == null || clientId == null){
+            log.debug("handleLeaveLobby: {} {}",errorMsg, missingParams.joinToString())
+            return Response.getError("$errorMsg ${missingParams.joinToString()}")
+        }
+        // === check if the lobby and player exist === |
+        val notFound = mutableListOf<String>()
+        val errorMsg2 = "The following were not found: "
+        /*(1)*/ val lobby = lobbies[lobbyId] ?: run { notFound.add("Lobby") ; null }
+        /*(2)*/ val clientHandler = clientController.getByClientId(clientId) ?: run { notFound.add("Player") ; null }
+        if(lobby == null || clientHandler == null){
+            log.debug("handleLeaveLobby: {} {}",errorMsg2, notFound.joinToString())
+            return Response.getError("$errorMsg2 ${notFound.joinToString()}")
+        }
+        // ======================================== |
 
         val success = lobby.remove(clientId)
         return if(success){
             clientToLobby.remove(clientId)
-
             // notify the client
             clientHandler.sendTcp(GameRequest.builder(Type.LEAVE_LOBBY).build().toJson())
-
+            log.debug("handleLeaveLobby() successful")
             Response.getOk()
         } else {
+            log.debug("handleLeaveLobby() failed: Player not in lobby")
             Response.getError("Player not in lobby")
         }
     }
 
     private fun handleJoinLobby(request: GameRequest) : String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val clientId = request.playerId ?: return Response.getError("Player ID not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
-        val clientHandler = clientController.getByClientId(clientId) ?: return Response.getError("Player not found")
+        log.debug("handleJoinLobby() with lobbyId: {}, playerId: {}",request.lobbyId,request.playerId)
+
+        // ========= parameter validation ========= |
+        val missingParams = mutableListOf<String>()
+        val errorMsg = "Missing the following parameters: "
+        /*(1)*/ val lobbyId = request.lobbyId ?: run { missingParams.add("lobbyId") ; null }
+        /*(2)*/ val clientId = request.playerId ?: run { missingParams.add("playerId") ; null }
+        if(lobbyId == null || clientId == null){
+            log.debug("handleJoinLobby: {} {}",errorMsg, missingParams.joinToString())
+            return Response.getError("$errorMsg ${missingParams.joinToString()}")
+        }
+        // === check if the lobby and player exist === |
+        val notFound = mutableListOf<String>()
+        val errorMsg2 = "The following were not found: "
+        /*(1)*/ val lobby = lobbies[lobbyId] ?: run { notFound.add("Lobby") ; null }
+        /*(2)*/ val clientHandler = clientController.getByClientId(clientId) ?: run { notFound.add("Player") ; null }
+        if(lobby == null || clientHandler == null){
+            log.debug("handleJoinLobby: {} {}",errorMsg2, notFound.joinToString())
+            return Response.getError("$errorMsg2 ${notFound.joinToString()}")
+        }
+        // ======================================== |
 
         // Try to add the player to the lobby
         val success = lobby.add(clientId)
 
         return if(success){
             clientToLobby[clientId] = lobbyId
-
             // notify the client
             clientHandler.sendTcp(
                 GameRequest.builder(Type.JOIN_LOBBY)
                     .lobbyId(lobbyId)
                     .gameType(lobby.gameType)
                     .build().toJson())
-
+            log.debug("handleJoinLobby() successful")
             Response.getOk()
         } else {
+            log.debug("handleJoinLobby() failed: Lobby is full")
             Response.getError("Lobby is full")
         }
     }
 
     private fun handleCreateLobby(request: GameRequest) : String {
-        val gameType = request.gameType ?: return Response.getError("Game type not given in request")
+        log.debug("handleCreateLobby() with gameType: {}", request.gameType)
+
+        // ========= parameter validation ========= |
+        val errorMsg = "Missing the following parameters: gameType"
+        val gameType = request.gameType ?: run {
+            log.debug("handleCreateLobby: {}",errorMsg)
+            return Response.getError(errorMsg)
+        }
+        // ======================================== |
+
         val lobbyId = idGenerator.generate()
         val lobby = Lobby(lobbyId,gameType)
         lobbies[lobbyId] = lobby
+        log.debug("handleCreateLobby() successful")
         return Response.getOk(lobbyId)
     }
 
     private fun handleEndGame(request: GameRequest) : String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
-        val game = games[lobbyId] ?: return Response.getError("Game not found")
+        log.debug("handleEndGame() with lobbyId: {}",request.lobbyId)
+
+        // ========= parameter validation ========= |
+        val errorMsg = "Missing the following parameters: lobbyId"
+        val lobbyId = request.lobbyId ?: run {
+            log.debug("handleEndGame: {}",errorMsg)
+            return Response.getError(errorMsg)
+        }
+        // === check if the lobby and game exist === |
+        val notFound = mutableListOf<String>()
+        val errorMsg2 = "The following were not found: "
+        /*(1)*/ val lobby = lobbies[lobbyId] ?: run { notFound.add("Lobby") ; null }
+        /*(2)*/ val game = games[lobbyId] ?: run { notFound.add("Game") ; null }
+        if(lobby == null || game == null){
+            log.debug("handleEndGame: {} {}",errorMsg2, notFound.joinToString())
+            return Response.getError("$errorMsg2 ${notFound.joinToString()}")
+        }
+        // ======================================== |
+
         game.endGame()
         games.remove(lobby.id)
         lobby.state = LobbyState.WAITING
+        // notify the clients
         lobby.getPlayers()
             .map {clientController.getByClientId(it)}
             .forEach {
@@ -142,35 +249,51 @@ class GameController(private val clientController: ClientController) {
                     .build().toJson()
             )
         }
+        log.debug("handleEndGame() successful")
         return Response.getOk()
     }
 
     private fun handleStartGame(request: GameRequest) : String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
+        log.debug("handleStartGame() with lobbyId: {}",request.lobbyId)
+
+        // ========= parameter validation ========= |
+        val errorMsg = "Missing the following parameters: lobbyId"
+        val lobbyId = request.lobbyId ?: run {
+            log.debug("handleStartGame: {}",errorMsg)
+            return Response.getError(errorMsg)
+        }
+        // === check if the lobby exists === |
+        val lobby = lobbies[lobbyId] ?: run {
+            log.debug("handleStartGame: Lobby not found")
+            return Response.getError("Lobby not found")
+        }
+        // ======================================== |
 
         // Check if the lobby is ready
-        if(lobby.isReady().not()) {
+        if(! lobby.isReady()) {
             return Response.getError("Lobby is not ready")
         }
 
-        val bad = Response.getError("Failed to start game")
-
-        // Start the game
+        // lobby is ready implies lobby has 2 players
         val player1Id = lobby.player1Id!!
         val player2Id = lobby.player2Id!!
-        val player1Handler = clientController.getByClientId(player1Id) ?: run{
-            log.error("Player 1 handler is null in lobby: $lobbyId")
-            return bad
+
+        // ========= player validation ========= |
+        val notFound = mutableListOf<String>()
+        val errorMsg2 = "The following were not found: "
+        /*(1)*/ val player1Handler = clientController.getByClientId(player1Id) ?: run{ notFound.add("Player 1 handler") ; null }
+        /*(2)*/ val player2Handler = clientController.getByClientId(player2Id) ?: run{ notFound.add("Player 2 handler") ; null }
+        if(player1Handler == null || player2Handler == null){
+            // should not happen
+            log.error("handleStartGame: {} {}",errorMsg2, notFound.joinToString())
+            return Response.getError("Failed to start game")
         }
-        val player2Handler = clientController.getByClientId(player2Id) ?: run{
-            log.error("Player 2 handler is null in lobby: $lobbyId")
-            return bad
-        }
+        // ======================================== |
+
         val game = when(lobby.gameType){
                 GameType.WATER_RIPPLES -> WaterRipplesGame(player1Handler, player2Handler)
                 else -> return Response.getError("Invalid game type")
-            }
+        }
         lobby.state = LobbyState.PLAYING
         games[lobby.id] = game
         clientIdToGame[player1Id] = game
@@ -182,15 +305,30 @@ class GameController(private val clientController: ClientController) {
         player1Handler.sendTcp(msg)
         player2Handler.sendTcp(msg)
 
+        log.debug("handleStartGame() successful")
         return Response.getOk()
     }
 
     private fun handleSetLobbyType(request: GameRequest): String {
-        val lobbyId = request.lobbyId ?: return Response.getError("Lobby ID not given in request")
-        val gameType = request.gameType ?: return Response.getError("Game type not given in request")
-        val lobby = lobbies[lobbyId] ?: return Response.getError("Lobby not found")
-        lobby.gameType = gameType
+        log.debug("handleSetLobbyType() with lobbyId: {}, gameType: {}",request.lobbyId,request.gameType)
 
+        // ========= parameter validation ========= |
+        val errorMsg = "Missing the following parameters: "
+        val missingParams = mutableListOf<String>()
+        /*(1)*/ val lobbyId = request.lobbyId ?: run { missingParams.add("lobbyId") ; null }
+        /*(2)*/ val gameType = request.gameType ?: run { missingParams.add("gameType") ; null }
+        if(lobbyId == null || gameType == null){
+            log.debug("handleSetLobbyType: {} {}",errorMsg, missingParams.joinToString())
+            return Response.getError("$errorMsg ${missingParams.joinToString()}")
+        }
+        // === check if the lobby exists === |
+        val lobby = lobbies[lobbyId] ?: run {
+            log.debug("handleSetLobbyType: Lobby not found")
+            return Response.getError("Lobby not found")
+        }
+        // ======================================== |
+
+        lobby.gameType = gameType
         // Notify the clients
         lobby.getPlayers()
             .map {clientController.getByClientId(it)}
@@ -202,6 +340,7 @@ class GameController(private val clientController: ClientController) {
             )
         }
 
+        log.debug("handleSetLobbyType() successful")
         return Response.getOk()
     }
 
