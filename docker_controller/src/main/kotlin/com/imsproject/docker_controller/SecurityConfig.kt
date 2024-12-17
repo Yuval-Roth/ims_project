0e25f8607435c8d6d98395a056a7a08415dad5d9
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-17 14:47:04 +0200

    Added security to docker controller

---
+++ docker_controller/src/main/kotlin/com/imsproject/docker_controller/SecurityConfig.kt
@@ -0,0 +1,48 @@
+package com.imsproject.docker_controller
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize.anyRequest().authenticated()
+            }
+            .formLogin(Customizer.withDefaults())
+            .httpBasic{ obj -> obj.disable() }
+            .authenticationManager(object : AuthenticationManager {
+
+                private val encoder: PasswordEncoder = BCryptPasswordEncoder()
+                private val pass = "\$2a\$10\$Ss09W28r0vuNd67EHqcAw.piDzMvPFV4YHK0d0rh2C30O26NYAewG"
+
+                override fun authenticate(authentication: Authentication): Authentication {
+                    val userName = authentication.name
+                    if(userName.lowercase() != "admin") {
+                        throw BadCredentialsException("Bad Credentials")
+                    }
+                    val password = authentication.credentials.toString()
+                    if (encoder.matches(password, pass)) {
+                        return UsernamePasswordAuthenticationToken(userName, password, emptyList())
+                    } else {
+                        throw BadCredentialsException("Bad Credentials")
+                    }
+                }
+            })
+        return http.build()
+    }
+}
