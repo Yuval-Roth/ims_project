8016755774171ae842cddea21a77002507cca99c
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-20 01:53:49 +0200

    updates

--- docker_controller/src/main/kotlin/com/imsproject/docker_controller/App.kt
+++ docker_controller/src/main/kotlin/com/imsproject/docker_controller/App.kt
@@ -2,21 +2,24 @@ package com.imsproject.docker_controller
 
 import com.google.gson.Gson
 import org.springframework.boot.web.servlet.error.ErrorController
-import org.springframework.core.io.ResourceLoader
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.net.URLEncoder
 import java.util.*
 
 @Controller
-class App (private val resources : ResourceLoader) : ErrorController {
+class App : ErrorController {
 
     @GetMapping("/")
-    fun home(model: Model) : String {
-        val proc : Process
+    fun home(model: Model): String {
+        val proc: Process
         try {
             proc = ProcessBuilder("docker ps --format json".split(" ")).start()
         } catch (e: Exception) {
@@ -51,7 +54,7 @@ class App (private val resources : ResourceLoader) : ErrorController {
     }
 
     @GetMapping("/docker/up")
-    fun dockerUp(model: Model): String{
+    fun dockerUp(model: Model): String {
         return runCommand(
             "docker compose -f /home/admin/compose.yml up --detach",
             "Docker services started successfully",
@@ -60,7 +63,7 @@ class App (private val resources : ResourceLoader) : ErrorController {
     }
 
     @GetMapping("/docker/down")
-    fun dockerDown(model: Model): String{
+    fun dockerDown(model: Model): String {
         return runCommand(
             "docker compose -f /home/admin/compose.yml down",
             "Docker services shutdown initiated successfully",
@@ -68,6 +71,14 @@ class App (private val resources : ResourceLoader) : ErrorController {
         )
     }
 
+    @GetMapping("/docker/restart")
+    fun dockerRestart(model: Model): String {
+        return runCommand(
+            "docker compose -f /home/admin/compose.yml restart",
+            "Docker services restarted successfully",
+            "Error restarting docker services"
+        )
+    }
 
     @GetMapping("/success")
     fun successPage(model: Model, @RequestParam(value = "msg") msg : String): String {
@@ -104,5 +115,53 @@ class App (private val resources : ResourceLoader) : ErrorController {
     private fun success(msg: String): String {
         val urlMsg = URLEncoder.encode(msg,"UTF-8")
         return "redirect:/success?msg=$urlMsg"
+    }
+
+
+}
+
+@RestController
+class LogsProvider {
+
+    @GetMapping("/logs/game-server")
+    fun gameServerLog(
+        @RequestParam(value = "rows", required = false, defaultValue = "-1") rows: Int
+    ): ResponseEntity<String> {
+        return readLog("/home/admin/volumes/game_server/logs/application.log", rows)
+    }
+
+    @GetMapping("/logs/manager")
+    fun managerLog(
+        @RequestParam(value = "rows", required = false, defaultValue = "-1") rows: Int
+    ): ResponseEntity<String> {
+        return readLog("/home/admin/volumes/manager/logs/output.log", rows)
+    }
+
+    fun readLog(path: String, rows:Int): ResponseEntity<String> {
+        try {
+            val logFile = File(path)
+            val file = logFile.readText()
+            return if (rows > 0) {
+                val split = file.split("\n")
+                split.takeLast(rows).joinToString("<br/>").toResponseEntity()
+            } else {
+                file.replace("\n", "<br/>").toResponseEntity()
+            }
+        } catch (e: Exception) {
+            return "Error while fetching log file:<br/>${
+                e.stackTraceToString()
+                    .replace("\n", "<br/>")
+            }"
+                .toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+
+    private fun String.toResponseEntity (errorCode: HttpStatusCode): ResponseEntity<String> {
+        return ResponseEntity.status(errorCode).body(this)
+    }
+
+    private fun String.toResponseEntity (): ResponseEntity<String> {
+        return ResponseEntity.ok(this)
     }
 }
