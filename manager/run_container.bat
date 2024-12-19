899edbbb16c7dad3a9e51f1e8c6dcd9d7fb1b3e2
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-19 13:52:13 +0200

    changed port

--- manager/run_container.bat
+++ manager/run_container.bat
@@ -1,4 +1,4 @@
 call docker build -t imsproject_manager .
 call docker container rm imsproject_manager
-docker run -p 80:80 --name imsproject_manager imsproject_manager
+docker run -p 5000:5000 --name imsproject_manager imsproject_manager
  
