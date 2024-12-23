d5ade962d6b94d3dcfbbe742ac1776de2f365951
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-12-23 14:13:05 +0200

    Added more logic to track the finger better

--- watch/app/src/main/java/com/imsproject/watch/view/WineGlassesActivity.kt
+++ watch/app/src/main/java/com/imsproject/watch/view/WineGlassesActivity.kt
@@ -1,27 +1,19 @@
 package com.imsproject.watch.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.awaitTouchSlopOrCancellation
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.*
@@ -30,39 +22,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.IntentSanitizer
 import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.MaterialTheme
 import com.imsproject.watch.DARK_BACKGROUND_COLOR
-import com.imsproject.watch.PACKAGE_PREFIX
 import com.imsproject.watch.SCREEN_WIDTH
 import com.imsproject.watch.initGlobalValues
 import com.imsproject.watch.textStyle
-import com.imsproject.watch.view.contracts.Result
-import com.imsproject.watch.viewmodel.GameViewModel
-import com.imsproject.watch.viewmodel.WineGlassesViewModel
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.PointerId
-import androidx.wear.compose.material.SwipeToDismissBox
-import androidx.wear.compose.material.rememberSwipeToDismissBoxState
 import com.imsproject.watch.LIGHT_BLUE_COLOR
-import com.imsproject.watch.WATER_RIPPLES_ANIMATION_DURATION
 import kotlin.math.atan2
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlinx.coroutines.delay
+import kotlin.math.absoluteValue
 
 val GLOW_COLOR = Color(0xFFFFA500) // Example orange glow color
 private const val MARKER_FADE_DURATION = 500
 private const val MARKER_SIZE = 30
+
+private const val UNDEFINED_ANGLE = 600f
 
 class WineGlassesActivity : ComponentActivity() {
 
@@ -142,17 +125,30 @@ class WineGlassesActivity : ComponentActivity() {
         }
     }
 
+    @SuppressLint("ReturnFromAwaitPointerEventScope")
     @Composable
     fun WineGlasses() {
         var touchPoint = remember { mutableStateOf<Pair<Double,Double>>(Pair(-1.0,-1.0)) }
+
+        // ============= touch point related values =========== |
         val center = remember {Offset(SCREEN_WIDTH / 2f, SCREEN_WIDTH / 2f)}
-        val radiusOuterEdge = remember{ (SCREEN_WIDTH / 2) }
+        val radiusOuterEdge = remember{ (SCREEN_WIDTH / 2).toFloat() }
         val radiusInnerEdge = remember { (SCREEN_WIDTH / 2) * 0.2f }
-        val radiusCenter = remember { (SCREEN_WIDTH / 2) * 0.7f }
+        val released = remember { mutableStateOf(false) }
+        var targetAngle = remember { mutableFloatStateOf(UNDEFINED_ANGLE) }
+        var currentAngle = remember { mutableFloatStateOf(UNDEFINED_ANGLE) }
+        // ===================================================== |
+
+        // ============= arc related values ================= |
         val defaultAlpha = remember { 0.8f }
         val alpha = remember { mutableFloatStateOf(0.8f) }
-        val counter = remember { mutableIntStateOf(0) }
-        val released = remember { mutableStateOf(false) }
+        val arcSize = remember { Size(radiusOuterEdge * 2, radiusOuterEdge * 2) }
+        val sweepAngle = remember { 30f }
+        var angleSkew = remember { 0f }
+        var previousAngle = remember { UNDEFINED_ANGLE }
+        var direction = remember { 0f }
+        var arcStartAngle = remember { mutableFloatStateOf(0f) }
+        // ================================================== |
 
         Box(
             modifier = Modifier
@@ -164,10 +160,8 @@ class WineGlassesActivity : ComponentActivity() {
                             val event = awaitPointerEvent()
                             if (event.type == PointerEventType.Release) {
                                 released.value = true
-                                counter.intValue++
                             } else {
                                 released.value = false
-                                alpha.floatValue = defaultAlpha
                                 val eventFirst = event.changes.first()
                                 eventFirst.consume()
                                 val touchPosition = eventFirst.position
@@ -179,11 +173,22 @@ class WineGlassesActivity : ComponentActivity() {
                 },
             contentAlignment = Alignment.Center
         ) {
-            LaunchedEffect(counter.intValue) {
+            LaunchedEffect(released.value) {
                 if(released.value){
                     val alphaAnimStep =  defaultAlpha / (MARKER_FADE_DURATION / 16f)
                     while(released.value && alpha.floatValue > 0.0f){
                         alpha.floatValue = (alpha.floatValue - alphaAnimStep).coerceAtLeast(0.0f)
+                        delay(16)
+                    }
+                    previousAngle = UNDEFINED_ANGLE
+                } else {
+                    alpha.floatValue = defaultAlpha
+                }
+            }
+            LaunchedEffect(released.value) {
+                if(! released.value) {
+                    while (true) {
+                        angleSkew = (angleSkew - 0.75f).coerceAtLeast(20f)
                         delay(16)
                     }
                 }
@@ -207,72 +212,65 @@ class WineGlassesActivity : ComponentActivity() {
                             (touchPoint.value.second - center.y).pow(2.0)
                 )
 
-                val angle = Math.toDegrees(
-                    atan2(
-                        touchPoint.value.second - center.y,
-                        touchPoint.value.first - center.x
-                    ).toDouble()
-                ).toFloat()
+                if(! released.value){
+                    var angle = Math.toDegrees(
+                        atan2(
+                            touchPoint.value.second - center.y,
+                            touchPoint.value.first - center.x
+                        ).toDouble()
+                    ).toFloat()
 
-                if (distanceFromCenter >= radiusInnerEdge && distanceFromCenter <= radiusOuterEdge) {
-                    if (touchPoint.value.first != -1.0 && touchPoint.value.second != -1.0) {
-                        // Define the arc size
-                        val arcRadius = radiusOuterEdge.toFloat() // Adjust as needed
-                        val arcSize = Size(arcRadius * 2, arcRadius * 2)
+                    //make the angle move faster with faster change rate
 
-                        // Calculate start angle and sweep angle
-                        val startAngle = angle - 15f // Adjust start angle
-                        val sweepAngle = 30f // Define arc length in degrees
-
-                        drawArc(
-                            color = GLOW_COLOR.copy(alpha = alpha.floatValue),
-                            startAngle = startAngle,
-                            sweepAngle = sweepAngle,
-                            useCenter = false, // Open arc, not filled
-                            topLeft = Offset(
-                                center.x - arcRadius,
-                                center.y - arcRadius
-                            ),
-                            size = arcSize,
-                            style = Stroke(width = MARKER_SIZE.dp.toPx()) // Adjust thickness
-                        )
+                    // ==== angle logic ==== |
+                    val skewedAngle = angle + direction * angleSkew // for current iteration
+                    if(previousAngle != UNDEFINED_ANGLE){ // for next iteration
+                        if ((angle - previousAngle).absoluteValue > 2){
+                            angleSkew = (angleSkew + 1f).coerceAtMost(40f)
+                        }
                     }
+                    // ====================== |
+
+                    // ==== direction logic ==== |
+                    if (previousAngle != UNDEFINED_ANGLE){
+                        println("angle: $angle")
+                        if(angle > 0){
+                            if(angle > previousAngle){
+                                direction += 0.1f
+                            } else if (angle < previousAngle){
+                                direction -= 0.1f
+                            }
+                        } else if (angle < 0){
+                            if(angle > previousAngle){
+                                direction += 0.1f
+                            } else if(angle < previousAngle){
+                                direction -= 0.1f
+                            }
+                        }
+                    }
+                    direction = direction.coerceIn(-1f,1f)
+                    // =============================== |
+
+                    previousAngle = angle
+                    arcStartAngle.floatValue = skewedAngle - sweepAngle / 2
+                }
+
+                // draw only if the touch point is within the defined borders
+                if (distanceFromCenter >= radiusInnerEdge && distanceFromCenter <= radiusOuterEdge) {
+                    drawArc(
+                        color = GLOW_COLOR.copy(alpha = alpha.floatValue),
+                        startAngle = arcStartAngle.floatValue,
+                        sweepAngle = sweepAngle,
+                        useCenter = false, // Open arc, not filled
+                        topLeft = Offset(
+                            center.x - radiusOuterEdge,
+                            center.y - radiusOuterEdge
+                        ),
+                        size = arcSize,
+                        style = Stroke(width = MARKER_SIZE.dp.toPx()) // Adjust thickness
+                    )
                 }
             }
-
-
-//            Canvas(modifier = Modifier.fillMaxSize()) {
-//
-//                // calculate distance from center
-//                // to determine if the touch point is roughly within the circle
-//                // we allow a little bit of leeway
-//                val distanceFromCenter = sqrt(
-//                    (touchPoint.value.first - center.x).pow(2.0) +
-//                            (touchPoint.value.second - center.y).pow(2.0))
-//
-//                //calculate angle from coordinates
-//                val angle = Math.toRadians(Math.toDegrees(
-//                    atan2(
-//                        touchPoint.value.second - center.y,
-//                        touchPoint.value.first - center.x
-//                    )
-//                ))
-//
-//                //calculate x and y to draw the circle
-//                val x = center.x + radiusCenter * kotlin.math.cos(angle).toFloat()
-//                val y = center.y + radiusCenter * kotlin.math.sin(angle).toFloat()
-//
-//                if(distanceFromCenter >= radiusInnerEdge && distanceFromCenter <= radiusOuterEdge){
-//                    if(touchPoint.value.first != -1.0 && touchPoint.value.second != -1.0){
-//                        drawCircle(
-//                            color = GLOW_COLOR.copy(alpha = alpha.floatValue),
-//                            center = Offset(x, y),
-//                            radius = MARKER_SIZE.dp.toPx(),
-//                            style = Fill
-//                        )
-//                    }
-//                }
-//            }
         }
     }
 
