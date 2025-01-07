a3fa01e3a2d4a6bac6972f844f5d7f0623f5ead2
Yuval Roth <rothyuv@post.bgu.ac.il>
2025-01-07 20:03:56 +0200

    updated docker push scripts

--- game_server/publish_docker_image.bat
+++ game_server/publish_docker_image.bat
@@ -1,18 +1,9 @@
-call mvn clean package -DskipTests
-call docker build -t yuvalroth123/imsproject_game_server .
-call docker tag imsproject_game_server yuvalroth123/imsproject_game_server:latest
-call docker push yuvalroth123/imsproject_game_server:latest
-
 @echo off
-echo:
-echo:
-echo:
-echo ================================================================================
-echo only yuval can push to this docker repository, so if it didn't work you know why
-echo ================================================================================
-echo:
-echo:
-echo:
+set image_name=imsproject_game_server
+
+call mvn clean package -DskipTests
+call docker build -t yuvalroth123/%image_name% .
+call docker push yuvalroth123/%image_name%:latest
 pause
 
 
