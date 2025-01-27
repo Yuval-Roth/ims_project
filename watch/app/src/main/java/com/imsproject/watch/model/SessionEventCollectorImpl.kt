a8d188cd38702efa47232d56c16720313cbc4fa6
Yuval Roth <rothyuv@post.bgu.ac.il>
2025-01-27 23:48:08 +0200

    removed prints

--- watch/app/src/main/java/com/imsproject/watch/model/SessionEventCollectorImpl.kt
+++ watch/app/src/main/java/com/imsproject/watch/model/SessionEventCollectorImpl.kt
@@ -9,9 +9,6 @@ class SessionEventCollectorImpl private constructor() : SessionEventCollector {
 
     override fun addEvent(event: SessionEvent) {
         events.add(event)
-        if(event.type == SessionEvent.Type.SENSOR_DATA) {
-            println(event)
-        }
     }
 
     override fun getAllEvents(): Collection<SessionEvent> {
