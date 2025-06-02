0d2a09c21e400c6e32333dc2636cc8b1b0c9a656
olgaoznovich <ol.oznovich@gmail.com>
2025-06-02 17:12:05 +0300

    added fade to items and changed spot to be the same

--- watch/app/src/main/java/com/imsproject/watch/Properties.kt
+++ watch/app/src/main/java/com/imsproject/watch/Properties.kt
@@ -123,7 +123,9 @@ var GRASS_PLANT_STROKE_WIDTH = 4.5f
 //shared
 var GRASS_WATER_RADIUS = 0f //initialized later
 var GRASS_WATER_ANGLE = 36
-var GRASS_WATER_VISIBILITY_THRESHOLD = 450
+var GRASS_WATER_VISIBILITY_THRESHOLD = 250
+var CURRENT_PLAYER_ITEM_ALPHA = 0.4f
+var OPPONENT_PLAYER_ITEM_ALPHA = 0.9f
 
 // ================== After game questions =============== |
 const val FIRST_QUESTION = "עד כמה חשת תחושת \"ביחד\" עם השותפ/ה במשחקון הזה?"
