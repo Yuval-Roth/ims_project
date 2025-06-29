0b178585f79f404e40d8417acff5e067a79dee8f
Tamir Avisar <tamirosh@post.bgu.ac.il>
2025-06-29 19:13:56 +0300

    add UI tests for operators checking

--- tests/2023-mbt-04-14/Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ tests/2023-mbt-04-14/Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -1,20 +1,17 @@
 package hellocucumber;
 
 import io.cucumber.java.en.*;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.Console;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
@@ -45,9 +42,19 @@ public class StepDefinitions {
     private final String Add_Participant_Save_Button= "//*[@id=\"participant-modal\"]/div/div/button[2]";
     // remove participant
     private final String Remove_Participant_Button = "//*[@id=\"remove-participant-btn\"]";
+    //navigate to operators
+    private final String Remove_Operator_Button = "//*[@id=\"remove-operator-btn\"]";
+    private final String operators_Button = "//*[@id=\"operators-btn\"]";
+    //add operator
+    private final String add_Operator = "//*[@id=\"add-operator-btn\"]";
+    private final String add_Operator_Username_Textbox = "//*[@id=\"username\"]";
+    private final String add_Operator_Password_Textbox = "//*[@id=\"password\"]";
+    private final String add_Operator_Save_Button = "//*[@id=\"save-operator-btn\"]";
 
     private final String username = "admin";
     private final String password = "pass";
+    private final String main_Menu_URL = "https://ims-project.cs.bgu.ac.il/main_menu";
+    private final String disconnect_Button = "//*[@id=\"disconnect-btn\"]";
 
     private String URL;
 
@@ -101,12 +108,13 @@ public class StepDefinitions {
 
     // -----------------------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------------
-    // First scenario: Manager adds new participant
+    // First scenario: Admin adds new participant.
     // -----------------------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------------
 
     @Given("A logged in admin user with username and password")
     public void adminLogin() throws InterruptedException {
+        System.out.println("\nhi1\n");
         driver = new ChromeDriver();
         driver.get(URL);
         Thread.sleep(2500);
@@ -211,7 +219,7 @@ public class StepDefinitions {
 
     // -----------------------------------------------------------------------------
     // -----------------------------------------------------------------------------
-    // Second scenario: Teacher reduces the maximum size of the file to submit to 1.
+    // Second scenario: Admin remove participant.
     // -----------------------------------------------------------------------------
     // -----------------------------------------------------------------------------
 
@@ -288,6 +296,217 @@ public class StepDefinitions {
         List<WebElement> emailCells = driver.findElements(By.cssSelector(Participants_Table));
         assertEquals(check_if_exists_record_with_email(emailCells, email), -1);
         Thread.sleep(2500);
+        driver.quit();
+    }
+
+
+    // -----------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
+    // 3rd scenario: Admin adds new operator.
+    // -----------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
+
+    @And("There is no operator with the username {string}")
+    public void thereIsNoOperatorWithTheUsername(String username) throws InterruptedException {
+        WebElement Operators_Button = driver.findElement(By.xpath(operators_Button));
+        Operators_Button.click();
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+        List<WebElement> adminCells = driver.findElements(By.cssSelector(Participants_Table));
+        int result = check_if_exists_record_with_email(adminCells, username);
+        if(result != -1){
+            adminCells.get(result).click();
+
+            //click on the delete button to remove the user
+            WebElement deleteButton = driver.findElement(By.xpath(Remove_Operator_Button));
+            deleteButton.click();
+
+            //accept the alert
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.alertIsPresent());
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+
+            //accept the second alert
+            WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait2.until(ExpectedConditions.alertIsPresent());
+            Alert alert2 = driver.switchTo().alert();
+            alert2.accept();
+
+        }
+    }
+
+    @When("Add operator with the username {string} and password {string}")
+    public void addOperator(String username, String password) throws InterruptedException {
+        WebElement addOperatorButton = driver.findElement(By.xpath(add_Operator));
+        addOperatorButton.click();
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        //add the details of the operator
+        WebElement UserNameButton = driver.findElement(By.xpath(add_Operator_Username_Textbox));
+        UserNameButton.sendKeys(username);
+        WebElement PasswordButton = driver.findElement(By.xpath(add_Operator_Password_Textbox));
+        PasswordButton.sendKeys(password);
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        WebElement saveButton = driver.findElement(By.xpath(add_Operator_Save_Button));
+        saveButton.click();
+
+        //handles the opened window
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+    }
+
+    @Then("The operator with username {string} added successfully")
+    public void theOperatorAddedSuccessfully(String username) throws InterruptedException {
+        List<WebElement> operatorsCells = driver.findElements(By.cssSelector(Participants_Table));
+        assertNotEquals(check_if_exists_record_with_email(operatorsCells, username), -1);
+        Thread.sleep(2500);
+    }
+
+    @And("The operator with username {string} and password {string} can log in")
+    public void operatorCanLogin(String username, String password) throws InterruptedException {
+        driver.get(main_Menu_URL);
+        try {
+            WebElement disconnectButton = driver.findElement(By.xpath(disconnect_Button));
+            disconnectButton.click();
+        }
+        catch (Exception ignored) {
+
+        }
+        WebElement UserNameButton = driver.findElement(By.xpath(Username_Text_Box));
+        UserNameButton.sendKeys(username);
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        WebElement PasswordButton = driver.findElement(By.xpath(Password_Text_Box));
+        PasswordButton.sendKeys(password);
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        WebElement loginButton = driver.findElement(By.xpath(LoginButton_In_Login_Page));
+        loginButton.click();
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        try {
+            WebElement disconnectButton = driver.findElement(By.xpath(disconnect_Button));
+            disconnectButton.click();
+            assertTrue(true);
+        }
+        catch (Exception ignored) {
+            fail();
+        }
+        driver.quit();
+    }
+
+    // -----------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
+    // 4th scenario: Admin removes operator.
+    // -----------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
+
+    @And("There is an operator with the username {string} and password {string}")
+    public void thereIsAnOperatorWithTheUsername(String username, String password) throws InterruptedException {
+        WebElement Operators_Button = driver.findElement(By.xpath(operators_Button));
+        Operators_Button.click();
+        System.out.println("\nhi2\n");
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        List<WebElement> adminCells = driver.findElements(By.cssSelector(Participants_Table));
+        int result = check_if_exists_record_with_email(adminCells, username);
+        System.out.println("\nhi " + Integer.toString(result) + "\n");
+        if(result == -1){
+            WebElement addOperatorButton = driver.findElement(By.xpath(add_Operator));
+            addOperatorButton.click();
+            Thread.sleep(DELAY_BETWEEN_STEPS);
+
+            //add the details of the operator
+            WebElement UserNameButton = driver.findElement(By.xpath(add_Operator_Username_Textbox));
+            UserNameButton.sendKeys(username);
+            WebElement PasswordButton = driver.findElement(By.xpath(add_Operator_Password_Textbox));
+            PasswordButton.sendKeys(password);
+            Thread.sleep(DELAY_BETWEEN_STEPS);
+
+            WebElement saveButton = driver.findElement(By.xpath(add_Operator_Save_Button));
+            saveButton.click();
+
+            //handles the opened window
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.alertIsPresent());
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+
+            Thread.sleep(DELAY_BETWEEN_STEPS);
+        }
+    }
+
+    @When("Remove the operator with the username {string}")
+    public void reomoveOperator(String username) throws InterruptedException {
+        List<WebElement> adminCells = driver.findElements(By.cssSelector(Participants_Table));
+        int result = check_if_exists_record_with_email(adminCells, username);
+        if(result != -1){
+            adminCells.get(result).click();
+
+            //click on the delete button to remove the user
+            WebElement deleteButton = driver.findElement(By.xpath(Remove_Operator_Button));
+            deleteButton.click();
+
+            //accept the alert
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.alertIsPresent());
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+
+            //accept the second alert
+            WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait2.until(ExpectedConditions.alertIsPresent());
+            Alert alert2 = driver.switchTo().alert();
+            alert2.accept();
+
+        }
+        else {
+            fail("the operator " + username + " was not found");
+        }
+    }
+
+    @Then("The operator with username {string} removed successfully")
+    public void theOperatorRemovedSuccessfully(String username) throws InterruptedException {
+        List<WebElement> operatorsCells = driver.findElements(By.cssSelector(Participants_Table));
+        assertEquals(check_if_exists_record_with_email(operatorsCells, username), -1);
+        Thread.sleep(2500);
+    }
+
+    @And("The operator with username {string} and password {string} can't log in")
+    public void operatorCantLogin(String username, String password) throws InterruptedException {
+        driver.get(main_Menu_URL);
+        try {
+            WebElement disconnectButton = driver.findElement(By.xpath(disconnect_Button));
+            disconnectButton.click();
+        }
+        catch (Exception ignored) {
+
+        }
+        WebElement UserNameButton = driver.findElement(By.xpath(Username_Text_Box));
+        UserNameButton.sendKeys(username);
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        WebElement PasswordButton = driver.findElement(By.xpath(Password_Text_Box));
+        PasswordButton.sendKeys(password);
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        WebElement loginButton = driver.findElement(By.xpath(LoginButton_In_Login_Page));
+        loginButton.click();
+        Thread.sleep(DELAY_BETWEEN_STEPS);
+
+        try {
+            WebElement disconnectButton = driver.findElement(By.xpath(disconnect_Button));
+            disconnectButton.click();
+            fail();
+        }
+        catch (Exception ignored) {
+            assertTrue(true);
+        }
         driver.quit();
     }
 
--- tests/2023-mbt-04-14/Cucumber/src/test/resources/hellocucumber/UpdateFilesLimit.feature
+++ tests/2023-mbt-04-14/Cucumber/src/test/resources/hellocucumber/UpdateFilesLimit.feature
@@ -1,6 +1,6 @@
 Feature: add and remove participants
 
-  Scenario Outline: Manager adds new participant
+  Scenario Outline: Admin adds new participant
     Given A logged in admin user with username and password
     And There is no participant with the email "<email>"
     When Add participant with the email "<email>"
@@ -9,7 +9,7 @@ Feature: add and remove participants
       | email                   |
       | tamirosh@post.bgu.ac.il |
 
-  Scenario Outline: Manager remove participant
+  Scenario Outline: Admin remove participant
     Given A logged in admin user with username and password
     And There is a participant with the email "<email>"
     When remove the participant with the email "<email>"
@@ -18,3 +18,22 @@ Feature: add and remove participants
       | email                   |
       | tamirosh@post.bgu.ac.il |
 
+  Scenario Outline: Admin adds new operator
+    Given A logged in admin user with username and password
+    And There is no operator with the username "<username>"
+    When Add operator with the username "<username>" and password "<password>"
+    Then The operator with username "<username>" added successfully
+    And The operator with username "<username>" and password "<password>" can log in
+    Examples:
+      | username | password |
+      | tamirosh | Pass1234 |
+
+  Scenario Outline: Admin removes operator
+    Given A logged in admin user with username and password
+    And There is an operator with the username "<username>" and password "<password>"
+    When Remove the operator with the username "<username>"
+    Then The operator with username "<username>" removed successfully
+    And The operator with username "<username>" and password "<password>" can't log in
+    Examples:
+      | username | password |
+      | tamirosh | Pass1234 |
