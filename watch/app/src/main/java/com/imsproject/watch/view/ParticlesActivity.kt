050a482448bc53ef10223e04c92640961f553d7a
yuval-roth <yuvalroth123@gmail.com>
2025-10-19 11:47:09 +0300

    updated pacman visuals

--- watch/app/src/main/java/com/imsproject/watch/view/ParticlesActivity.kt
+++ watch/app/src/main/java/com/imsproject/watch/view/ParticlesActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -62,7 +63,7 @@ class ParticlesActivity: GameActivity(GameType.PARTICLES) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(color = Color.White),
+                .background(color = Color.Black),
             contentAlignment = Alignment.Center
         ) {
             val infiniteTransition = rememberInfiniteTransition(label = "rotation")
@@ -77,153 +78,140 @@ class ParticlesActivity: GameActivity(GameType.PARTICLES) {
                 label = "angle"
             )
             val currentOpeningAngle = angle - 90f
+            val pacmanMouthOpeningAnimation by infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 66f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = 200, easing = LinearEasing),
+                    repeatMode = RepeatMode.Reverse
+                ),
+                label = "pacmanMouthOpeningAnimation"
+            )
 
             // visual parameters for the mechanical wheel
-            val ringRadius = SCREEN_RADIUS * 0.5f
-            val ringThickness = SCREEN_RADIUS * 0.08f
-            val openingAngle = 40f
-            val sweepAngle = 360f - openingAngle
-            val teethCount = 12
-            val toothDepth = ringThickness * 1.25f
-            val toothWidthDegrees = sweepAngle / teethCount * 0.7f // spacing
-            val startAngle = -90f + openingAngle / 2
-            val toothAngleOffset = 4f // visual tuning, change this as needed to align the teeth to the opening
-            val toothWidthRads = Math.toRadians(toothWidthDegrees.toDouble())
+            val ringRadius = SCREEN_RADIUS * 0.15f
+            val sweepAngle = 360f
+            val startAngle = -90f
 
-            // stationary particles on both sides of the screen
+            // stationary particle on both sides of the screen
             val particleRadius = (SCREEN_RADIUS * 0.02f)
-            val particleRandomOffsetX = (SCREEN_RADIUS * 0.08f)
-            val particleRandomOffsetY = (SCREEN_RADIUS * 0.08f)
-            val offsetRangeX = -particleRandomOffsetX..particleRandomOffsetX
-            val offsetRangeY = -particleRandomOffsetY..particleRandomOffsetY
             val particleDistanceFromCenter = SCREEN_RADIUS * 0.88f
             val cageStrokeWidth = 4f
-
-            // clump of particles on each side in random arrangement
-            val particlesPerClump = 30
-            val leftClumpOffsets = remember {
-                List(particlesPerClump) {
-                    Offset(
-                        x = -particleDistanceFromCenter + offsetRangeX.random(),
-                        y = offsetRangeY.random()
-                    )
-                }
-            }
-            val rightClumpOffsets = remember {
-                List(particlesPerClump) {
-                    Offset(
-                        x = particleDistanceFromCenter + offsetRangeX.random(),
-                        y = offsetRangeY.random()
-                    )
-                }
-            }
+            val cageColor = Color(0xFF0000ff)
 
             Canvas(modifier = Modifier.fillMaxSize()) {
 
                 // draw cages for the particles
                 // left side
                 drawLine(
-                    color = Color.Black,
+                    color = cageColor,
                     start = Offset(0f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
                     end = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
                     strokeWidth = cageStrokeWidth,
                     cap = StrokeCap.Round
                 )
                 drawLine(
-                    color = Color.Black,
+                    color = cageColor,
+                    start = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f, SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
+                    end = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.175f),
+                    strokeWidth = cageStrokeWidth,
+                    cap = StrokeCap.Round
+                )
+                drawLine(
+                    color = cageColor,
                     start = Offset(0f, SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
                     end = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
                     strokeWidth = cageStrokeWidth,
                     cap = StrokeCap.Round
                 )
+                drawLine(
+                    color = cageColor,
+                    start = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
+                    end = Offset(SCREEN_CENTER.x - SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.175f),
+                    strokeWidth = cageStrokeWidth,
+                    cap = StrokeCap.Round
+                )
                 // right side
                 drawLine(
-                    color = Color.Black,
+                    color = cageColor,
                     start = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
                     end = Offset(size.width , SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
                     strokeWidth = cageStrokeWidth,
                     cap = StrokeCap.Round
                 )
                 drawLine(
-                    color = Color.Black,
+                    color = cageColor,
+                    start = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.12f),
+                    end = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y - SCREEN_RADIUS * 0.175f),
+                    strokeWidth = cageStrokeWidth,
+                    cap = StrokeCap.Round
+                )
+                drawLine(
+                    color = cageColor,
                     start = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
                     end = Offset(size.width , SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
                     strokeWidth = cageStrokeWidth,
                     cap = StrokeCap.Round
                 )
-
-                // center expanding circle
-                drawCircle(
-                    color = Color(0xFF9459EE),
-                    radius = SCREEN_RADIUS * 0.075f,
-                    center = SCREEN_CENTER
+                drawLine(
+                    color = cageColor,
+                    start = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.12f),
+                    end = Offset(SCREEN_CENTER.x + SCREEN_RADIUS * 0.76f , SCREEN_CENTER.y + SCREEN_RADIUS * 0.175f),
+                    strokeWidth = cageStrokeWidth,
+                    cap = StrokeCap.Round
                 )
 
-                // mechanical wheel
+                // draw stationary particles
+                // left side
+                drawRoundRect(
+                    color = Color.White,
+                    topLeft = Offset(
+                        x = SCREEN_CENTER.x - particleDistanceFromCenter - particleRadius,
+                        y = SCREEN_CENTER.y - particleRadius
+                    ),
+                    size = Size(particleRadius * 2, particleRadius * 2),
+                    cornerRadius = CornerRadius(2f, 2f)
+                )
+                // right side
+                drawRoundRect(
+                    color = Color.White,
+                    topLeft = Offset(
+                        x = SCREEN_CENTER.x + particleDistanceFromCenter - particleRadius,
+                        y = SCREEN_CENTER.y - particleRadius
+                    ),
+                    size = Size(particleRadius * 2, particleRadius * 2),
+                    cornerRadius = CornerRadius(2f, 2f)
+                )
+
+
+                // rotating pacman
                 rotate(angle) {
-                    // ring
+                    // pacman body
                     drawArc(
-                        color = Color.Black,
-                        startAngle = startAngle,
-                        sweepAngle = sweepAngle,
-                        useCenter = false,
+                        color = Color.Yellow,
+                        startAngle = startAngle + pacmanMouthOpeningAnimation / 2f,
+                        sweepAngle = sweepAngle - pacmanMouthOpeningAnimation,
+                        useCenter = true,
                         topLeft = Offset(SCREEN_CENTER.x - ringRadius, SCREEN_CENTER.y - ringRadius),
                         size = Size(ringRadius * 2, ringRadius * 2),
-                        style = Stroke(width = ringThickness)
                     )
-                    // teeth
-                    for (i in 0 until teethCount) {
-                        val toothAngle = startAngle + toothAngleOffset + (i * (sweepAngle / teethCount))
-                        val rad = Math.toRadians(toothAngle.toDouble())
-                        val toothWidthOffset = rad + toothWidthRads
-                        val innerRadius = ringRadius + ringThickness / 2
-                        val outerRadius = innerRadius + toothDepth
-
-                        // trapezoid corners
-                        val p1 = Offset(
-                            x = SCREEN_CENTER.x + innerRadius * cos(rad).toFloat(),
-                            y = SCREEN_CENTER.y + innerRadius * sin(rad).toFloat()
-                        )
-                        val radp2 = rad + 0.08f // trapezoid narrower at outer edge
-                        val p2 = Offset(
-                            x = SCREEN_CENTER.x + outerRadius * cos(radp2).toFloat(),
-                            y = SCREEN_CENTER.y + outerRadius * sin(radp2).toFloat()
-                        )
-                        val radp3 = toothWidthOffset - 0.08f  // trapezoid narrower at outer edge
-                        val p3 = Offset(
-                            x = SCREEN_CENTER.x + outerRadius * cos(radp3).toFloat(),
-                            y = SCREEN_CENTER.y + outerRadius * sin(radp3).toFloat()
-                        )
-                        val p4 = Offset(
-                            x = SCREEN_CENTER.x + innerRadius * cos(toothWidthOffset).toFloat(),
-                            y = SCREEN_CENTER.y + innerRadius * sin(toothWidthOffset).toFloat()
-                        )
-                        val toothPath = Path().apply {
-                            moveTo(p1.x, p1.y)
-                            lineTo(p2.x, p2.y)
-                            lineTo(p3.x, p3.y)
-                            lineTo(p4.x, p4.y)
-                            close()
-                        }
-                        drawPath(
-                            path = toothPath,
-                            color = Color.Black
-                        )
-                    }
-                }
-
-                leftClumpOffsets.forEach {
+                    // pacman eye
+                    val eyeOffsetFromCenter = ringRadius * 0.6f
                     drawCircle(
-                        color = Color(0xFFFF4141),
-                        radius = particleRadius.toFloat(),
-                        center = Offset(SCREEN_CENTER.x + it.x, SCREEN_CENTER.y + it.y)
+                        color = Color.White,
+                        radius = ringRadius * 0.2f,
+                        center = Offset(
+                            x = SCREEN_CENTER.x + eyeOffsetFromCenter * cos(Math.toRadians(180.0).toFloat()),
+                            y = SCREEN_CENTER.y + eyeOffsetFromCenter * sin(Math.toRadians(180.0).toFloat())
+                        )
                     )
-                }
-                rightClumpOffsets.forEach {
                     drawCircle(
-                        color = Color(0xFF3B82F6),
-                        radius = particleRadius.toFloat(),
-                        center = Offset(SCREEN_CENTER.x + it.x, SCREEN_CENTER.y + it.y)
+                        color = Color.Black,
+                        radius = ringRadius * 0.1f,
+                        center = Offset(
+                            x = SCREEN_CENTER.x + eyeOffsetFromCenter * cos(Math.toRadians(180.0).toFloat()),
+                            y = SCREEN_CENTER.y + eyeOffsetFromCenter * sin(Math.toRadians(180.0).toFloat())
+                        )
                     )
                 }
             }
