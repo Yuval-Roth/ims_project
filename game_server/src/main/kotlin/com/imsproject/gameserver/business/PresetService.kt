dd6a1a1367f45134b8b7fa9c2b50751b4cab4aed
Yuval Roth <yuvalroth123@gmail.com>
2025-10-27 11:24:59 +0200

    Fixed reindexing of sessions when deleting, added logs in PresetService

--- game_server/src/main/kotlin/com/imsproject/gameserver/business/PresetService.kt
+++ game_server/src/main/kotlin/com/imsproject/gameserver/business/PresetService.kt
@@ -1,9 +1,10 @@
 package com.imsproject.gameserver.business
 
-import com.imsproject.common.dataAccess.DaoException
 import com.imsproject.gameserver.dataAccess.implementations.PresetPK
 import com.imsproject.gameserver.dataAccess.implementations.PresetsDAO
 import com.imsproject.gameserver.dataAccess.models.PresetDTO
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,7 +16,9 @@ class PresetService(
     }
 
     fun addPreset(presetName: String) {
+        log.debug("Adding new preset: $presetName")
         if(dao.exists(PresetPK(presetName, 0))){
+            log.error("Error adding new preset: $presetName already exists")
             throw IllegalArgumentException("Preset with name $presetName already exists.")
         }
         val obj = PresetDTO(
@@ -23,20 +26,36 @@ class PresetService(
             sessions = emptyList()
         )
         dao.insert(obj)
+        log.debug("Preset added $presetName")
     }
 
     fun updatePreset(preset: PresetDTO) {
+        log.debug("Updating preset: ${preset.name}")
         if(! dao.exists(PresetPK(preset.name, -1))){
+            log.error("Error updating preset: preset ${preset.name} doesn't exist.")
             throw IllegalArgumentException("Preset with name ${preset.name} doesn't exist.")
         }
         dao.update(preset)
+        val configurationLogMessage = if(preset.sessions == null || preset.sessions.isEmpty()) {
+            "No sessions found"
+        } else {
+            preset.sessions.joinToString("\n\t")
+        }
+        log.debug("Preset updated: ${preset.name}\n\tnew preset configuration:\n\t$configurationLogMessage")
     }
 
     fun deletePreset(presetName: String) {
+        log.debug("Deleting preset: $presetName")
         if(! dao.exists(PresetPK(presetName, 0))){
+            log.error("Error deleting preset: $presetName doesn't exist.")
             throw IllegalArgumentException("Preset with name $presetName doesn't exist.")
         }
         val key = PresetPK(presetName,-1) // index is irrelevant for deletion of entire preset
         dao.delete(key)
+        log.debug("Preset removed $presetName")
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(PresetService::class.java)
     }
 }
